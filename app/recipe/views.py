e51bfd41791ddaf9259f46a6823121177d48d77b
Cristian Hernandez <chinorubikguitar@hotmail.com>
2023-05-14 14:28:26 -0600

    testing for deleting an ingredient and adding mixin to pass test

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -53,7 +53,9 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')    
 
 
-class IngredientViewSet(mixins.UpdateModelMixin,
+class IngredientViewSet(
+                        mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
                         mixins.ListModelMixin,
                         viewsets.GenericViewSet,
                         ):
